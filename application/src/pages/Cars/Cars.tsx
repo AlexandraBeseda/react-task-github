4ff09a9d1fa98b0e0ee7cd1343256946ed9dc965
AlexandraBeseda <s.ani.aa862@gmail.com>
2021-11-16 14:49:17 +0300

    s=>style

--- application/src/pages/Cars/Cars.tsx
+++ application/src/pages/Cars/Cars.tsx
@@ -1,21 +1,21 @@
 import React from 'react';
 import { redirect } from '../../bll/redirect';
 import cars from '../../data/database.json';
-import s from './Cars.module.css';
+import style from './Cars.module.css';
 
 export const Cars: React.FC = () => {
   redirect();
   return (
-    <div className={s.main}>
+    <div className={style.main}>
       {cars.map((car) => (
-        <div className={s.car} key={car.id}>
-          <img className={s.img} src={`${car.picture}`} alt="car" />
-          <div className={s.description}>
+        <div className={style.car} key={car.id}>
+          <img className={style.img} src={`${car.picture}`} alt="car" />
+          <div className={style.description}>
             <h2>{`Model: ${car.brand} ${car.model}`}</h2>
             <h3>{`Price: ${car.price}$`}</h3>
             <p>{`Colour: ${car.colour}`}</p>
             <p>{`Length x Width x Height: ${car.length} x ${car.width} x ${car.height}`}</p>
-            <p className={s.text}>{`Description: ${car.description}`}</p>
+            <p className={style.text}>{`Description: ${car.description}`}</p>
           </div>
         </div>
       ))}
